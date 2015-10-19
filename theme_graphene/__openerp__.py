4f14106e5682e4fdf7cc2a10eb05178de3199041
Michael Karrer <michaelkarrer81@gmail.com>
2015-10-19 12:16:40 +0200

    theme graphene

---
+++ theme_graphene/__openerp__.py
@@ -0,0 +1,21 @@
+{
+    'name':'Graphene theme',
+    'description': 'Light colours, thin text, clean and sharp design.',
+    'category': 'Theme/Corporate',
+    'version':'1.0',
+    'author':'Odoo S.A.',
+    'data': [
+        'views/assets.xml',
+        'views/customize_modal.xml',
+        'views/snippets.xml',
+        'views/options.xml',
+        'views/images_library.xml',
+        ],
+    'images': [
+        'static/description/graphene_poster.jpg',
+    ],
+    'depends': ['theme_common', 'snippet_google_map', 'website_animate', 'snippet_latest_posts'],
+    'price': 199,
+    'currency': 'EUR',
+    'live_test_url': 'https://theme-graphene.odoo.com'
+}
